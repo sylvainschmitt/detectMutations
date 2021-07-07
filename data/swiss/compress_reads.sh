480d1848f58875ae90f296030aa907503462e9e6
sylvainschmitt <sylvain.schmitt@agroparistech.fr>
2021-07-07 12:05:51 -0300

    compress reads

---
+++ data/swiss/compress_reads.sh
@@ -0,0 +1,4 @@
+module purge ; gzip lower_1.fastq
+module purge ; gzip lower_2.fastq
+module purge ; gzip upper_1.fastq
+module purge ; gzip upper_2.fastq
--- data/swiss/merge_reads.sh
+++ data/swiss/merge_reads.sh
@@ -1,4 +1,4 @@
-module purge ; zcat SRR386*_1.fastq.gz > lower_1.fastq.gz
-module purge ; zcat SRR386*_2.fastq.gz > lower_2.fastq.gz
-module purge ; zcat SRR388*_1.fastq.gz > upper_1.fastq.gz
-module purge ; zcat SRR388*_2.fastq.gz > upper_2.fastq.gz
+module purge ; zcat SRR386*_1.fastq.gz > lower_1.fastq
+module purge ; zcat SRR386*_2.fastq.gz > lower_2.fastq
+module purge ; zcat SRR388*_1.fastq.gz > upper_1.fastq
+module purge ; zcat SRR388*_2.fastq.gz > upper_2.fastq
