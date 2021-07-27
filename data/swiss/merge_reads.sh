45f8571645b93b76095926a418306b09c53b4c56
sylvainschmitt <sylvain.schmitt@agroparistech.fr>
2021-07-27 13:31:53 -0300

    get data with R

--- data/swiss/merge_reads.sh
+++ data/swiss/merge_reads.sh
@@ -1,4 +1,4 @@
-module purge ; zcat SRR386*_1.fastq.gz > lower_1.fastq
-module purge ; zcat SRR386*_2.fastq.gz > lower_2.fastq
-module purge ; zcat SRR388*_1.fastq.gz > upper_1.fastq
-module purge ; zcat SRR388*_2.fastq.gz > upper_2.fastq
+zcat SRR3884561_1.fastq.gz SRR3884562_1.fastq.gz SRR3884563_1.fastq.gz SRR3884564_1.fastq.gz | gzip > lower_1.fastq.gz ; rm SRR3884561_1.fastq.gz SRR3884562_1.fastq.gz SRR3884563_1.fastq.gz SRR3884564_1.fastq.gz
+zcat SRR3860149_1.fastq.gz SRR3860174_1.fastq.gz SRR3860329_1.fastq.gz SRR3860429_1.fastq.gz | gzip > upper_1.fastq.gz ; rm SRR3860149_1.fastq.gz SRR3860174_1.fastq.gz SRR3860329_1.fastq.gz SRR3860429_1.fastq.gz
+zcat SRR3884561_2.fastq.gz SRR3884562_2.fastq.gz SRR3884563_2.fastq.gz SRR3884564_2.fastq.gz | gzip > lower_2.fastq.gz ; rm SRR3884561_2.fastq.gz SRR3884562_2.fastq.gz SRR3884563_2.fastq.gz SRR3884564_2.fastq.gz
+zcat SRR3860149_2.fastq.gz SRR3860174_2.fastq.gz SRR3860329_2.fastq.gz SRR3860429_2.fastq.gz | gzip > upper_2.fastq.gz ; rm SRR3860149_2.fastq.gz SRR3860174_2.fastq.gz SRR3860329_2.fastq.gz SRR3860429_2.fastq.gz
